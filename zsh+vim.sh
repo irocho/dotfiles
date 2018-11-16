647f383fce377f2f9e79838f9fad57789b1a4a07
irocho <irocho.profe@gmail.com>
2018-11-16 09:35:57 +0100

    mover ficheiros de conf

--- zsh+vim.sh
+++ zsh+vim.sh
@@ -11,7 +11,7 @@ echo "==================================="
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-
+cd .
 cp zshrc /home/$USER/.zshrc
 echo "==================================="
 echo "zsh coas súas fontes debería ir"
