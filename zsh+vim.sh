dd3feb80b7c05b167f986ea4548ee5fedc159072
irocho <irocho.profe@gmail.com>
2018-11-21 09:47:26 +0100

    recolocados

--- zsh+vim.sh
+++ zsh+vim.sh
@@ -1,28 +1,31 @@
-apt-get update && apt-get upgrade -y
-apt-get install zsh -y
-apt-get install vim -y
-apt-get install git -y
-apt-get install curl -y
-apt-get install wget -y
+sudo apt-get update && apt-get upgrade -y
+sudo apt-get install zsh -y
+sudo apt-get install vim -y
+sudo apt-get install git -y
+sudo apt-get install curl -y
+sudo apt-get install wget -y
 echo "==================================="
 echo "Instalados paquetes básicos"
 echo "==================================="
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
-cd .
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-cp zshrc /home/$USER/.zshrc
-echo "==================================="
-echo "zsh coas súas fontes debería ir"
-echo "==================================="
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp vimrc /home/$USER/.vimrc
+cp ./vimrc /home/$USER/.vimrc
 vim +PlugInstall +qall
 vim -c 'PluginInstall' -c'source %' -c 'qa!'
 echo "==========================================="
 echo "vim listo cos seus plugins"
 echo "==========================================="
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~
+
+cp /home/$USER/dotfiles/zshrc /home/$USER/.zshrc
+echo "==================================="
+echo "zsh coas súas fontes debería ir"
+echo "==================================="
+
